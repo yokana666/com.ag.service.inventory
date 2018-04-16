e4eec172ef1e441643d830b6886312f0160e10b1
jackyruslymoonlay <jacky.rusly@moonlay.com>
2018-04-16 15:45:48 +0700

    Material Distribution Note Unit Test

---
+++ Com.Danliris.Service.Inventory.Test/DataUtils/IntegrationDataUtil/ProductDataUtil.cs
@@ -0,0 +1,28 @@
+﻿using Com.Danliris.Service.Inventory.Lib.Helpers;
+using Com.Danliris.Service.Inventory.Lib.ViewModels;
+using Com.Danliris.Service.Inventory.Test.Helpers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Com.Danliris.Service.Inventory.Test.DataUtils.IntegrationDataUtil
+{
+    public static class ProductDataUtil
+    {
+        public static ProductViewModel GetProduct(HttpClientService client)
+        {
+            var responseProduct = client.GetAsync($"{APIEndpoint.Core}/master/products?page=1&size=1").Result;
+            responseProduct.EnsureSuccessStatusCode();
+
+            var dataProduct = responseProduct.Content.ReadAsStringAsync();
+            Dictionary<string, object> resultProduct = JsonConvert.DeserializeObject<Dictionary<string, object>>(dataProduct.Result.ToString());
+
+            List<ProductViewModel> listProduct = JsonConvert.DeserializeObject<List<ProductViewModel>>(resultProduct["data"].ToString());
+            ProductViewModel product = listProduct.First();
+
+            return product;
+        }
+    }
+}
