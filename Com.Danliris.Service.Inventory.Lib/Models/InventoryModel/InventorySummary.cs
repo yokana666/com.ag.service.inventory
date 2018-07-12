fc88b84385220c37d6d1a70ef62b20051bc82ab4
achensetyawati <achenzetya@gmail.com>
2018-07-12 10:25:00 +0700

    models

---
+++ Com.Danliris.Service.Inventory.Lib/Models/InventoryModel/InventorySummary.cs
@@ -0,0 +1,24 @@
+﻿using Com.Moonlay.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Inventory.Lib.Models.InventoryModel
+{
+    public class InventorySummary : StandardEntity
+    {
+        public string No { get; set; }
+        public string ProductId { get; set; }
+        public string ProductCode { get; set; }
+        public string ProductName { get; set; }
+
+        public string UomUnit { get; set; }
+        public string UomId { get; set; }
+
+        public int StorageId { get; set; }
+        public string StorageCode { get; set; }
+        public string StorageName { get; set; }
+        public double Quantity { get; set; }
+        public double StockPlanning { get; set; }
+    }
+}
