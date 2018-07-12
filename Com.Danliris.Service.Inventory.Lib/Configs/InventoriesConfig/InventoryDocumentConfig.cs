471e4cc751cb40b578668f8ba113936894f16740
achensetyawati <achenzetya@gmail.com>
2018-07-12 10:24:27 +0700

    configs

---
+++ Com.Danliris.Service.Inventory.Lib/Configs/InventoriesConfig/InventoryDocumentConfig.cs
@@ -0,0 +1,29 @@
+﻿using Com.Danliris.Service.Inventory.Lib.Models.InventoryModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Inventory.Lib.Configs.InventoriesConfig
+{
+    public class InventoryDocumentConfig : IEntityTypeConfiguration<InventoryDocument>
+    {
+        public void Configure(EntityTypeBuilder<InventoryDocument> builder)
+        {
+            builder.Property(p => p.No).HasMaxLength(255);
+            builder.Property(p => p.ReferenceNo).HasMaxLength(255);
+            builder.Property(p => p.ReferenceType).HasMaxLength(255);
+            builder.Property(p => p.StorageId).HasMaxLength(255);
+            builder.Property(p => p.StorageCode).HasMaxLength(255);
+            builder.Property(p => p.StorageName).HasMaxLength(255);
+
+            builder
+                .HasMany(h => h.Items)
+                .WithOne(w => w.InventoryDocument)
+                .HasForeignKey(f => f.InventoryDocumentId)
+                .IsRequired();
+        }
+    }
+}
+
---
+++ Com.Danliris.Service.Inventory.Lib/Configs/InventoriesConfig/InventoryMovementConfig.cs
@@ -0,0 +1,27 @@
+﻿using Com.Danliris.Service.Inventory.Lib.Models.InventoryModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Inventory.Lib.Configs.InventoriesConfig
+{
+    public class InventoryMovementConfig : IEntityTypeConfiguration<InventoryMovement>
+    {
+        public void Configure(EntityTypeBuilder<InventoryMovement> builder)
+        {
+            builder.Property(p => p.No).HasMaxLength(255);
+            builder.Property(p => p.ReferenceNo).HasMaxLength(255);
+            builder.Property(p => p.ReferenceType).HasMaxLength(255);
+            builder.Property(p => p.StorageId).HasMaxLength(255);
+            builder.Property(p => p.StorageCode).HasMaxLength(255);
+            builder.Property(p => p.StorageName).HasMaxLength(255);
+            builder.Property(p => p.ProductId).HasMaxLength(255);
+            builder.Property(p => p.ProductCode).HasMaxLength(1000);
+            builder.Property(p => p.ProductName).HasMaxLength(4000);
+            builder.Property(p => p.UomUnit).HasMaxLength(255);
+            builder.Property(p => p.UomId).HasMaxLength(255);
+        }
+    }
+}
