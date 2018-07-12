f69165d659ca9e775870a9622cfa96049ed680b9
achensetyawati <achenzetya@gmail.com>
2018-07-12 10:25:12 +0700

    viewModels

---
+++ Com.Danliris.Service.Inventory.Lib/ViewModels/InventoryViewModel/InventoryMovementViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Com.Danliris.Service.Inventory.Lib.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Inventory.Lib.ViewModels.InventoryViewModel
+{
+    public class InventoryMovementViewModel : BasicViewModel
+    {
+        public string no { get; set; }
+        public DateTimeOffset date { get; set; }
+        public string referenceNo { get; set; }
+        public string referenceType { get; set; }
+        public string productId { get; set; }
+        public string productCode { get; set; }
+        public string productName { get; set; }
+
+        public string uomUnit { get; set; }
+        public string uomId { get; set; }
+
+        public int storageId { get; set; }
+        public string storageCode { get; set; }
+        public string storageName { get; set; }
+
+        public double stockPlanning { get; set; }
+
+        public double before { get; set; }
+        public double quantity { get; set; }
+        public double after { get; set; }
+        public string remark { get; set; }
+        public string type { get; set; }
+    }
+}
