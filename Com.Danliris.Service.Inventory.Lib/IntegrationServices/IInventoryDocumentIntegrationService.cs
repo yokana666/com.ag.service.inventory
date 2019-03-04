e3dc289e88bf1402f8f3a9654d9422e956e511e8
leslieaula <cristiano.ulo@gmail.com>
2019-03-04 15:27:06 +0700

    Add Inventory Integration Service

---
+++ Com.Danliris.Service.Inventory.Lib/IntegrationServices/IInventoryDocumentIntegrationService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Com.Danliris.Service.Inventory.Lib.IntegrationServices
+{
+    public interface IInventoryDocumentIntegrationService
+    {
+        Task<int> IntegrateData();
+    }
+}
