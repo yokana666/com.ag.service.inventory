8c6e48d10f9a503a8514b4b0e21a80ba89716e1c
ali-moonlay <43127426+ali-moonlay@users.noreply.github.com>
2019-06-25 14:43:03 +0700

    fix unit test fp return to inv

--- Com.Danliris.Service.Inventory.Test/DataUtils/FPReturnInvToPurchasingDataUtil/FPReturnInvToPurchasingDataUtil.cs
+++ Com.Danliris.Service.Inventory.Test/DataUtils/FPReturnInvToPurchasingDataUtil/FPReturnInvToPurchasingDataUtil.cs
@@ -1,50 +1,79 @@
-﻿namespace Com.Danliris.Service.Inventory.Test.DataUtils.FPReturnInvToPurchasingDataUtil
+﻿using Com.Danliris.Service.Inventory.Lib.Models.FPReturnInvToPurchasingModel;
+using Com.Danliris.Service.Inventory.Lib.Services.FpRegradingResultDocs;
+using Com.Danliris.Service.Inventory.Lib.Services.FPReturnInvToPurchasingService;
+using Com.Danliris.Service.Inventory.Lib.ViewModels;
+using Com.Danliris.Service.Inventory.Lib.ViewModels.FPReturnInvToPurchasingViewModel;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Com.Danliris.Service.Inventory.Test.DataUtils.FPReturnInvToPurchasingDataUtil
 {
-    //public class FPReturnInvToPurchasingDataUtil : BaseDataUtil<FPReturnInvToPurchasing, FPReturnInvToPurchasingFacade>, IEmptyData<FPReturnInvToPurchasingViewModel>
-    //{
-    //    //private readonly FpRegradingResultDataUtil.FpRegradingResultDataUtil fpRegradingResultDataUtil;
-    //    private readonly FPReturnInvToPurchasingDetailDataUtil fpReturnInvToPurchasingDetailDataUtil;
+    public class FPReturnInvToPurchasingDataUtil
+    {
+        //private readonly FpRegradingResultDataUtil.FpRegradingResultDataUtil fpRegradingResultDataUtil;
+        //private readonly FPReturnInvToPurchasingDetailDataUtil fpReturnInvToPurchasingDetailDataUtil;
+        private readonly NewFPReturnInvToPurchasingService Service;
+        private readonly NewFpRegradingResultDocsService FPRService;
+        public FPReturnInvToPurchasingDataUtil(NewFPReturnInvToPurchasingService service, NewFpRegradingResultDocsService fprService)
+        {
+            Service = service;
+            FPRService = fprService;
+        }
 
-    //    public FPReturnInvToPurchasingDataUtil(FPReturnInvToPurchasingFacade facade, FpRegradingResultDataUtil.FpRegradingResultDataUtil fpRegradingResultDataUtil, FPReturnInvToPurchasingDetailDataUtil fpReturnInvToPurchasingDetailDataUtil) : base(facade)
-    //    {
-    //        this.fpRegradingResultDataUtil = fpRegradingResultDataUtil;
-    //        this.fpReturnInvToPurchasingDetailDataUtil = fpReturnInvToPurchasingDetailDataUtil;
-    //    }
+        public FPReturnInvToPurchasingViewModel GetEmptyData()
+        {
+            return new FPReturnInvToPurchasingViewModel
+            {
+                Unit = new UnitViewModel(),
+                Supplier = new SupplierViewModel(),
+                FPReturnInvToPurchasingDetails = new List<FPReturnInvToPurchasingDetailViewModel>() { new FPReturnInvToPurchasingDetailViewModel() {
+                    Product = new ProductViewModel()
+                } }
+            };
+        }
 
-    //    public FPReturnInvToPurchasingViewModel GetEmptyData()
-    //    {
-    //        return new FPReturnInvToPurchasingViewModel
-    //        {
-    //            Unit = new UnitViewModel(),
-    //            Supplier = new SupplierViewModel(),
-    //            FPReturnInvToPurchasingDetails = new List<FPReturnInvToPurchasingDetailViewModel>() {  new FPReturnInvToPurchasingDetailViewModel() }
-    //        };
-    //    }
+        public FPReturnInvToPurchasing GetNewData()
+        {
+            
+            FPReturnInvToPurchasing TestData = new FPReturnInvToPurchasing
+            {
+                UnitName = "unit",
+                SupplierId = "1",
+                SupplierName = "name",
+                SupplierCode = "code",
+                FPReturnInvToPurchasingDetails = new List<FPReturnInvToPurchasingDetail>()
+                {
+                    new FPReturnInvToPurchasingDetail()
+                    {
+                        FPRegradingResultDocsId = 1,
+                        FPRegradingResultDocsCode = "code",
+                        ProductId = "1",
+                        ProductCode = "coode",
+                        ProductName = "name",
+                        Quantity = 1,
+                        NecessaryLength = 1,
+                        Length = 1,
+                        Description = ""
+                    }
+                }
+            };
 
-    //    public override FPReturnInvToPurchasing GetNewData()
-    //    {
-    //        Task<FpRegradingResultDocs> fpRegradingResultDocs = Task.Run(() => fpRegradingResultDataUtil.GetTestData());
-    //        fpRegradingResultDocs.Wait();
-
-    //        FPReturnInvToPurchasing TestData = new FPReturnInvToPurchasing
-    //        {
-    //            UnitName = fpRegradingResultDocs.Result.UnitName,
-    //            SupplierId = fpRegradingResultDocs.Result.SupplierId,
-    //            SupplierName = fpRegradingResultDocs.Result.SupplierName,
-    //            SupplierCode = fpRegradingResultDocs.Result.SupplierCode,
-    //            FPReturnInvToPurchasingDetails = fpReturnInvToPurchasingDetailDataUtil.GetNewData(fpRegradingResultDocs.Result)
-    //        };
-
-    //        return TestData;
-    //    }
+            return TestData;
+        }
 
 
-    //    public override async Task<FPReturnInvToPurchasing> GetTestData()
-    //    {
-    //        FPReturnInvToPurchasing Data = GetNewData();
-
-    //        await this.Facade.Create(Data);
-    //        return Data;
-    //    }
-    //}
+        public async Task<FPReturnInvToPurchasing> GetTestData()
+        {
+            FpRegradingResultDataUtil.FpRegradingResultDataUtil fprsDataUtil = new FpRegradingResultDataUtil.FpRegradingResultDataUtil(FPRService);
+            var regradDocs = await fprsDataUtil.GetTestData();
+            FPReturnInvToPurchasing Data = GetNewData();
+            foreach(var item in Data.FPReturnInvToPurchasingDetails)
+            {
+                item.FPRegradingResultDocsId = regradDocs.Id;
+                item.FPRegradingResultDocsCode = regradDocs.Code;
+            }
+            await this.Service.CreateAsync(Data);
+            return Data;
+        }
+    }
 }
